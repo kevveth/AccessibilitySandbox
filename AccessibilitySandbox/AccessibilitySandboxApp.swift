63260dfef4b357f9de0c1054f28b4687cce2d94e
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-05-13 23:45:18 -0700

    Initial Commit

---
+++ AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Kenneth Oliver Rathbun on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
